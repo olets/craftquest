aa999a49e0ef16536b5d68690b56546d42d12c30
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2021-06-29 14:28:28 -1000

    My first Craft CMS module, lesson 4: Using an Event to Customize the Craft Control Panel...
    https://craftquest.io/courses/my-first-craft-cms-module/31216

--- modules/cqcontrolpanel/CQControlPanel.php
+++ modules/cqcontrolpanel/CQControlPanel.php
@@ -3,12 +3,27 @@
 namespace craftquest;
 
 use Craft;
+use craft\events\RegisterCpNavItemsEvent;
+use craft\web\twig\variables\Cp;
 use Yii\base\Module;
+use Yii\base\Event;
 
 class CQControlPanel extends Module
 {
   public function init()
   {
+    Craft::setAlias('@cqcontrolpanel', __DIR__);
     parent::init();
+    Event::on(
+      Cp::class,
+      Cp::EVENT_REGISTER_CP_NAV_ITEMS,
+      function(RegisterCpNavItemsEvent $event) {
+        $event->navItems[] = [
+          'url' => 'entries/podcast',
+          'label' => 'Podcast Episodes',
+          'icon' => '@cqcontrolpanel/web/img/fa-microphone.svg',
+        ];
+      }
+    );
   }
 }
