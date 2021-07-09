81369507c63dbbc9a87563ab1fb7099dcaea409e
Henry Bley-Vroman <olets@olets.dev>
2021-07-08 14:32:24 -1000

    Building a Craft CMS dashboard widget, lesson 4: Customizing the Widget

--- modules/widgetopia/src/widgets/DeprecWidget.php
+++ modules/widgetopia/src/widgets/DeprecWidget.php
@@ -3,7 +3,26 @@
 namespace widgetopia\widgets;
 
 use craft\base\Widget;
+use Craft;
+use craft\helpers\UrlHelper;
+
 
 class DeprecWidget extends Widget {
 
+    public static function displayName(): string
+    {
+        return "Deprecation Warnings";
+    }
+
+    public function getTitle(): string
+    {
+        return "";
+    }
+
+    public function getBodyHtml(): string
+    {
+        $count = Craft::$app->getDeprecator()->getTotalLogs();
+        $deprecUrl = UrlHelper::url('utilities/deprecations-errors');
+        return "<div class='centeralign'><h2 style='font-size: 3rem;'>{$count}</h2><p>Deprecation warnings to address</p><p><a href='{$deprecUrl}' class='btn small'>Review and Fix</a></p></div>";
+    }
 }
